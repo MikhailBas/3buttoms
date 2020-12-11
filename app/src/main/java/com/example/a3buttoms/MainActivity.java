c344c4e613488bc0e73cb8e69265c89caefe0d03
MikhailBas <75811371+MikhailBas@users.noreply.github.com>
2020-12-11 03:18:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a3buttoms/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a3buttoms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        val b1 = findViewById < Button > (R.id.button1)
+
+
+
+
+           }
+}
